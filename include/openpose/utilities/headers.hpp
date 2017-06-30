dab9663c63906878fb201bacd88282f4b371b5f7
superying <slsuperrabbit@gmail.com>
2017-06-30 16:28:52 -0400

    init

---
+++ include/openpose/utilities/headers.hpp
@@ -0,0 +1,19 @@
+#ifndef OPENPOSE_UTILITIES_HEADERS_HPP
+#define OPENPOSE_UTILITIES_HEADERS_HPP
+
+// utilities module
+#include "check.hpp"
+#include "cuda.hpp"
+#include "enumClasses.hpp"
+#include "errorAndLog.hpp"
+#include "fastMath.hpp"
+#include "fileSystem.hpp"
+#include "flagsToOpenPose.hpp"
+#include "keypoint.hpp"
+#include "macros.hpp"
+#include "openCv.hpp"
+#include "pointerContainer.hpp"
+#include "profiler.hpp"
+#include "string.hpp"
+
+#endif // OPENPOSE_UTILITIES_HEADERS_HPP
